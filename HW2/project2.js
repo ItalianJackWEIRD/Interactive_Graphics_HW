aaa139a147d53c205494e8027c7754d316939325
ItalianJackWEIRD <jackybox93@gmail.com>
2026-03-27 18:48:53 +0100

    HW2: step 1 finito, step 2 iniziato
    Arrivato a setMesh e relative inizializzazioni

--- HW2/project2.js
+++ HW2/project2.js
@@ -3,8 +3,7 @@
 // It returns the combined 4x4 transformation matrix as an array in column-major order.
 // The given projection matrix is also a 4x4 matrix stored as an array in column-major order.
 // You can use the MatrixMult function defined in project4.html to multiply two 4x4 matrices in the same format.
-function GetModelViewProjection( projectionMatrix, translationX, translationY, translationZ, rotationX, rotationY )
-{
+function GetModelViewProjection(projectionMatrix, translationX, translationY, translationZ, rotationX, rotationY) {
 	// [TO-DO] Modify the code below to form the transformation matrix.
 	var trans = [
 		1, 0, 0, 0,
@@ -12,21 +11,43 @@ function GetModelViewProjection( projectionMatrix, translationX, translationY, t
 		0, 0, 1, 0,
 		translationX, translationY, translationZ, 1
 	];
-	var mvp = MatrixMult( projectionMatrix, trans );
+
+	// Rotation around x-axis
+	var cx = Math.cos(rotationX);
+	var sx = Math.sin(rotationX);
+	var rotX = [
+		1, 0, 0, 0,
+		0, cx, sx, 0,
+		0, -sx, cx, 0,
+		0, 0, 0, 1
+	];
+
+	// Rotation around y-axis
+	var cy = Math.cos(rotationY);
+	var sy = Math.sin(rotationY);
+	var rotY = [
+		cy, 0, -sy, 0,
+		0, 1, 0, 0,
+		sy, 0, cy, 0,
+		0, 0, 0, 1
+	];
+
+	// final matrix to be returned
+	var mvp = MatrixMult(projectionMatrix, MatrixMult(trans, MatrixMult(rotY, rotX)));
 	return mvp;
 }
 
 
 // [TO-DO] Complete the implementation of the following class.
 
-class MeshDrawer
-{
+class MeshDrawer {
 	// The constructor is a good place for taking care of the necessary initializations.
-	constructor()
-	{
-		// [TO-DO] initializations
+	constructor() {
+		// "setMesh" Initialization: Create the vertex buffer and the texture coordinate buffer;
+		this.vertexBuffer = gl.createBuffer();
+		this.texCoordBuffer = gl.createBuffer();
 	}
-	
+
 	// This method is called every time the user opens an OBJ file.
 	// The arguments of this function is an array of 3D vertex positions
 	// and an array of 2D texture coordinates.
@@ -37,49 +58,49 @@ class MeshDrawer
 	// Similarly, every two consecutive elements in the texCoords array
 	// form the texture coordinate of a vertex.
 	// Note that this method can be called multiple times.
-	setMesh( vertPos, texCoords )
-	{
-		// [TO-DO] Update the contents of the vertex buffer objects.
-		this.numTriangles = vertPos.length / 3;
+	setMesh(vertPos, texCoords) {
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.STATIC_DRAW);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
+
+		this.numTriangles = vertPos.length / 3;	// number of vertex/3 = number of triangles
 	}
-	
+
 	// This method is called when the user changes the state of the
 	// "Swap Y-Z Axes" checkbox. 
 	// The argument is a boolean that indicates if the checkbox is checked.
-	swapYZ( swap )
-	{
+	swapYZ(swap) {
 		// [TO-DO] Set the uniform parameter(s) of the vertex shader
 	}
-	
+
 	// This method is called to draw the triangular mesh.
 	// The argument is the transformation matrix, the same matrix returned
 	// by the GetModelViewProjection function above.
-	draw( trans )
-	{
+	draw(trans) {
 		// [TO-DO] Complete the WebGL initializations before drawing
 
-		gl.drawArrays( gl.TRIANGLES, 0, this.numTriangles );
+		gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles);
 	}
-	
+
 	// This method is called to set the texture of the mesh.
 	// The argument is an HTML IMG element containing the texture data.
-	setTexture( img )
-	{
+	setTexture(img) {
 		// [TO-DO] Bind the texture
 
 		// You can set the texture image data using the following command.
-		gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img );
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, img);
 
 		// [TO-DO] Now that we have a texture, it might be a good idea to set
 		// some uniform parameter(s) of the fragment shader, so that it uses the texture.
 	}
-	
+
 	// This method is called when the user changes the state of the
 	// "Show Texture" checkbox. 
 	// The argument is a boolean that indicates if the checkbox is checked.
-	showTexture( show )
-	{
+	showTexture(show) {
 		// [TO-DO] set the uniform parameter(s) of the fragment shader to specify if it should use the texture.
 	}
-	
+
 }
