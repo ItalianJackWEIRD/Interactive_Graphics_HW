99462d5e8795e1fa617f8222ff17cdfe598beac9
ItalianJackWEIRD <jackybox93@gmail.com>
2026-04-07 19:04:30 +0200

    step 2 progetto 2 fatto, manca step 3

--- HW2/project2.js
+++ HW2/project2.js
@@ -38,14 +38,42 @@ function GetModelViewProjection(projectionMatrix, translationX, translationY, tr
 }
 
 
+
+
+// SHADERS:
+var meshVS = `
+	attribute vec3 pos;		
+	uniform mat4 mvp;
+	uniform bool swapYZ;
+	void main() {
+		vec3 p = pos;
+		if (swapYZ) {
+			p = vec3(p.x, p.z, p.y);
+		}
+		gl_Position = mvp * vec4(p, 1.0);
+	} 
+	`;
+
+
+var meshFS = `
+	precision mediump float;
+	void main() {
+		gl_FragColor = vec4(1.0, gl_FragCoord.z * gl_FragCoord.z, 0.0, 1.0);	// rosso = 1.0, blu = 0.0
+	}`;
+
+
 // [TO-DO] Complete the implementation of the following class.
 
 class MeshDrawer {
 	// The constructor is a good place for taking care of the necessary initializations.
 	constructor() {
-		// "setMesh" Initialization: Create the vertex buffer and the texture coordinate buffer;
-		this.vertexBuffer = gl.createBuffer();
-		this.texCoordBuffer = gl.createBuffer();
+		// "setMesh" Initialization: Create the vertex buffer and the shader program, and get the locations of the shader parameters.
+		this.prog = InitShaderProgram(meshVS, meshFS);
+		this.mvpLoc = gl.getUniformLocation(this.prog, 'mvp');
+		this.posLoc = gl.getAttribLocation(this.prog, 'pos');
+		this.swapYZLoc = gl.getUniformLocation(this.prog, 'swapYZ');
+		this.vertBuffer = gl.createBuffer();
+		this.numTriangles = 0;
 	}
 
 	// This method is called every time the user opens an OBJ file.
@@ -59,20 +87,18 @@ class MeshDrawer {
 	// form the texture coordinate of a vertex.
 	// Note that this method can be called multiple times.
 	setMesh(vertPos, texCoords) {
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertPos), gl.STATIC_DRAW);
-
-		gl.bindBuffer(gl.ARRAY_BUFFER, this.texCoordBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texCoords), gl.STATIC_DRAW);
-
-		this.numTriangles = vertPos.length / 3;	// number of vertex/3 = number of triangles
+		this.numTriangles = vertPos.length / 3;	// number of triangles
 	}
 
 	// This method is called when the user changes the state of the
 	// "Swap Y-Z Axes" checkbox. 
 	// The argument is a boolean that indicates if the checkbox is checked.
 	swapYZ(swap) {
-		// [TO-DO] Set the uniform parameter(s) of the vertex shader
+		// [TO-DO] set the uniform parameter(s) of the vertex shader to specify if it should swap y and z axes.
+		gl.useProgram(this.prog);
+		gl.uniform1i(this.swapYZLoc, swap ? 1 : 0);
 	}
 
 	// This method is called to draw the triangular mesh.
@@ -80,6 +106,12 @@ class MeshDrawer {
 	// by the GetModelViewProjection function above.
 	draw(trans) {
 		// [TO-DO] Complete the WebGL initializations before drawing
+
+		gl.useProgram(this.prog);
+		gl.uniformMatrix4fv(this.mvpLoc, false, trans);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.vertBuffer);
+		gl.vertexAttribPointer(this.posLoc, 3, gl.FLOAT, false, 0, 0);
+		gl.enableVertexAttribArray(this.posLoc);
 
 		gl.drawArrays(gl.TRIANGLES, 0, this.numTriangles);
 	}
